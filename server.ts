b781250f0574267b3a7c6c3eab71c1fa20b70b44
xenofront <stsachidis@gmail.com>
2022-11-02 00:42:36 +0200

    debug

--- server.ts
+++ server.ts
@@ -1,16 +1,14 @@
 import { blue, yellow } from "https://deno.land/std@0.157.0/fmt/colors.ts";
-import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-// import { appController } from "./src/controllers/mod.ts";
+import { Application } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { appController, botController } from "./src/controllers/mod.ts";
 
 const PORT = Number(Deno.env.get("PORT") || 3000);
 
 const app = new Application();
-const router = new Router();
-app.use(router.routes());
 
-// app.use(appController.prefix("/test").routes());
-// app.use(botController.prefix("/bot").routes());
-
-app.listen({ port: PORT });
+app.use(appController.prefix("/test").routes());
+app.use(botController.prefix("/bot").routes());
 
 console.log(blue(`app listening on port ${yellow(String(PORT))}`));
+
+await app.listen({ port: PORT });
