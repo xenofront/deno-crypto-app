1a29be059fbb249f3431729405a97e8dc14d223d
xenofront <stsachidis@gmail.com>
2022-11-06 17:14:45 +0200

    test github workflow

---
+++ mod.ts
@@ -0,0 +1,14 @@
+import { blue, yellow } from "std/fmt/colors.ts";
+import { Application } from "oak";
+import { appController, botController } from "controllers/mod.ts";
+
+const PORT = Number(Deno.env.get("PORT") || 3000);
+
+const app = new Application();
+
+app.use(appController.prefix("/test").routes());
+app.use(botController.prefix("/bot").routes());
+
+console.log(blue(`app listening on port ${yellow(String(PORT))}`));
+
+await app.listen({ port: PORT });
