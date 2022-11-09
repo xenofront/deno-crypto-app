19a60cec682dad5ae387ee73575e9d842ec48b0a
xenofront <stsachidis@gmail.com>
2022-11-09 23:19:10 +0200

    workflow on main branch

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const PORT = Number(Deno.env.get("PORT") || 3000);
 
 const app = new Application();
 
-app.use(appController.prefix("/test").routes());
+app.use(appController.prefix("/app").routes());
 app.use(botController.prefix("/bot").routes());
 
 console.log(blue(`app listening on port ${yellow(PORT.toString())}`));
--- src/controllers/app/app.controller.ts
+++ src/controllers/app/app.controller.ts
@@ -3,7 +3,7 @@ import { Context, Router } from "oak";
 const router = new Router();
 
 router.get("/", (ctx: Context) => {
-  ctx.response.body = "hello";
+  ctx.response.body = "alive";
   ctx.response.status = 200;
 });
 
