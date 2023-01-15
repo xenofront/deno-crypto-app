a5486188d658d82e892ecd6473a2468cd8128209
xenofront <stsachidis@gmail.com>
2023-01-15 19:18:23 +0200

    introduce dev env

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,13 @@ import { registerControllers } from "controllers/mod.ts";
 const PORT = Number(Deno.env.get("PORT") || 3000);
 
 const app = new Application();
-
 registerControllers(app);
 
-console.log(blue(`http://localhost:${yellow(PORT.toString())}`));
+const isDev = Deno.env.get("ENVIRONMENT") === "dev";
+console.log(
+  blue(
+    isDev ? `http://localhost:${yellow(PORT.toString())}` : "app is listening",
+  ),
+);
 
 await app.listen({ port: PORT });
