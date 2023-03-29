940994e8879dfd7de031a0e1ae0362c3e89aa3e1
xenofront <stsachidis@gmail.com>
2023-03-30 00:05:25 +0300

    json error

--- src/controllers/telegram-bot/bot.service.ts
+++ src/controllers/telegram-bot/bot.service.ts
@@ -55,7 +55,7 @@ class BotService {
         `${coinGeckoUri}/price?ids=${ids}&vs_currencies=usd`,
       );
       const tokens: ITokenRes = await res.json();
-      console.log(tokens, "tokens");
+      console.log(tokens);
       const coins: ICoin[] = JSON.parse(Deno.env.get("COINS") as string);
 
       return coins.map((x) => {
