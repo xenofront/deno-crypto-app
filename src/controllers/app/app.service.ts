cf5191e36d8a6d4f92f03a66ed73acc8d02c7548
Xenophon Tsachidis <xenophon.tsachidis@telekom.com>
2023-03-30 12:06:37 +0300

    express

--- src/controllers/app/app.service.ts
+++ src/controllers/app/app.service.ts
@@ -2,13 +2,13 @@
 
 class AppService {
   public async appTest(res) {
-    res.status(200).send("ok");
     const result = await fetch(
       "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
     );
 
     const tokens: ITokenRes = await result.json();
     console.log(tokens);
+    res.status(200).send(tokens);
   }
 }
 
